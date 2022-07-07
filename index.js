cda2728aeb5d564beae314b3974144d7a231c0fe
Vint <vint.winchester@gmail.com>
2022-07-08 00:35:59 +0300

    Playing with envs

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ import cookie from 'cookie';
 import http from 'http';
 
 const PORT = process.env.PORT || 5000;
-let apiFlow = process.env.npm_package_version + (process.env.NODE_API_SUFFIX || '-l0');
-console.log(apiFlow);
+// let apiFlow = process.env.npm_package_version + (process.env.NODE_API_SUFFIX || '-l0');
+const apiFlow = () => {
+    return process.env.npm_package_version + (process.env.NODE_API_SUFFIX || '-l0');
+}
+console.log(apiFlow());
 
 const launch = () => {
     return http.createServer((req, res) => {
 
         if (req.url === '/') {
 
-            res.setHeader('Set-Cookie', cookie.serialize('api', apiFlow));
+            res.setHeader('Set-Cookie', cookie.serialize('api', apiFlow()));
 
             res.writeHead(200);
-            res.end(`Main page release: ${apiFlow}`);
+            res.end(`Main page release: ${apiFlow()}`);
         }
     });
 }
@@ -24,8 +27,8 @@ launch().listen(PORT, 'localhost', () => {
 });
 
 process.on('SIGHUP', () => {
-    apiFlow = process.env.npm_package_version + process.env.NODE_API_SUFFIX;
-    const sec2End = 15000;
+    // apiFlow = process.env.npm_package_version + process.env.NODE_API_SUFFIX;
+    const sec2End = 30000;
     console.log(`Exiting in ${sec2End}`);
     setTimeout(() => {
         process.exit(0);
