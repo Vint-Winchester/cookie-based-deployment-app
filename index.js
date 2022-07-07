8b283610705634b0fcab4c7bd3ff81dc8bb27f3c
Vint <vint.winchester@gmail.com>
2022-07-08 01:47:03 +0300

    Hell changes

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ import cookie from 'cookie';
 import http from 'http';
 
 const PORT = process.env.PORT || 5000;
-// let apiFlow = process.env.npm_package_version + (process.env.NODE_API_SUFFIX || '-l0');
-const apiFlow = () => {
-    return process.env.npm_package_version + (process.env.NODE_API_SUFFIX || '-l0');
-}
-console.log(apiFlow());
+let apiFlow = process.env.npm_package_version + '-l' + (process.env.NODE_API_SUFFIX || 0);
+console.log(apiFlow);
 
 const launch = () => {
     return http.createServer((req, res) => {
 
         if (req.url === '/') {
 
-            res.setHeader('Set-Cookie', cookie.serialize('api', apiFlow()));
+            res.setHeader('Set-Cookie', cookie.serialize('api', apiFlow));
 
             res.writeHead(200);
-            res.end(`Main page release: ${apiFlow()}`);
+            res.end(`Main page release: ${apiFlow}`);
         }
     });
 }
@@ -27,7 +24,7 @@ launch().listen(PORT, 'localhost', () => {
 });
 
 process.on('SIGHUP', () => {
-    // apiFlow = process.env.npm_package_version + process.env.NODE_API_SUFFIX;
+    apiFlow = process.env.npm_package_version + '-l' + +!process.env.NODE_API_SUFFIX;
     const sec2End = 30000;
     console.log(`Exiting in ${sec2End}`);
     setTimeout(() => {
