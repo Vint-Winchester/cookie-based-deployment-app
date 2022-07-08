81242f62be672390b151b4e18846478226379790
Vint <vint.winchester@gmail.com>
2022-07-08 11:59:37 +0300

    suffix correction

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 5000;
 let apiFlow = process.env.npm_package_version + '-l' + (process.env.NODE_API_SUFFIX || 0);
 console.log('start', apiFlow);
 
-// const upApiFlow = () => {
-//     apiFlow = process.env.npm_package_version + '-l' + +!process.env.NODE_API_SUFFIX;
-// };
-
 const launch = () => {
     return http.createServer((req, res) => {
         console.log('inside', apiFlow);
@@ -29,8 +25,7 @@ launch().listen(PORT, 'localhost', () => {
 
 process.on('SIGHUP', () => {
     console.log('suffix:', process.env.NODE_API_SUFFIX);
-    apiFlow = process.env.npm_package_version + '-l' + +!process.env.NODE_API_SUFFIX;
-    // upApiFlow();
+    apiFlow = process.env.npm_package_version + '-l' + +!+process.env.NODE_API_SUFFIX;
     console.log('switch', apiFlow);
     const sec2End = 30000;
     console.log(`Exiting in ${sec2End}`);
